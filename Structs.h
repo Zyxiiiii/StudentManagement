617209b236611588b5920d153e03bab64d53b4a6
funvi <2634075018@qq.com>
2022-06-18 14:02:08 +0800

    fix(comment):将注释改为英文

--- Structs.h
+++ Structs.h
@@ -46,12 +46,22 @@ StudentList ReadStudent();
 // 设置课程成绩
 void SetClassScore(int studentId, String className, float score);
 
-// 获取课程成绩
+/**/
 float GetClassScore(int studentId, String className);
 
-// 将学生添加到链表中
+/*
+	insert a new student into the student list
+
+	@param student: new student
+	@param studentList: the student list
+*/
 void AddStudentToList(Student student, StudentList studentList);
 
-// 根据学号获取学生
+/*
+	get student from a student list by id
+
+	@param id: the id of the student which u want
+	@param studentList: the student list
+*/
 Student* GetStudent(int id, StudentList studentList);
 #endif // !STUDENT_STRUCT_FUNCTION
